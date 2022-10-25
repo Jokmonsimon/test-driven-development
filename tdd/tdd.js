66fc0352efcc85318fb220abc11a414061349dde
Afande Ojok <simonpeter.oj@gmail.com>
2022-10-25 11:05:09 +0300

    Test file

--- tdd/tdd.js
+++ tdd/tdd.js
@@ -1,12 +1,25 @@
-describe.only('the fahrenheit to celsius canary spec', () => {
-    it('shows the infrastructure works', () => {
-        true.should.be.true();
+function hahrenheit2celsius(fahrenheit){
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function describe(){
+    describe.only('the fahrenheit to celsius canary spec', () => {
+        it('shows the infrastructure works', () => {
+            true.should.be.true();
+        })
+        describe('fahrenheit to celsius converter behaves as follows', () => {
+            it('212 degrees fahrenheit is 100 degrees celsius', () => {
+                hahrenheit2celsius(212).should.be.equal(100);
+            });
+            it('32 degrees fahrenheit is 0 degrees celsius', () => {
+                hahrenheit2celsius(32).should.be.equal(0);
+            });
+            it('50 degrees fahrenheit is 10 degrees celsius', () => {
+                hahrenheit2celsius(50).should.be.equal(10);
+            });
+            it('-22 degrees fahrenheit is -30 degrees celsius', () => {
+                hahrenheit2celsius(-22).should.be.equal(-30);
+            });
+        })
     })
-    describe('fahrenheit to celsius converter behaves as follows', () => {
-        it('212 degrees fahrenheit is 100 degrees celsius', () => {
-            fahrenheit2celsius(212).should.be.equal(100);
-        });
-        it('32 degrees fahrenheit is 0 degrees celsius');
-        it('50 degrees fahrenheit is 10 degrees celsius')
-    })
-})
+}
